7aa6dc6a436d6e98841362590907486ca2f3b6a4
MuhammadAtiq757 <muhammadatiq757@gmail.com>
2024-02-02 21:46:15 +0600

    up

--- src/Pages/Contact/About/About.jsx
+++ src/Pages/Contact/About/About.jsx
@@ -9,12 +9,12 @@ const About = () => {
 <p className='border-b-4 border-black w-[60px] mx-auto mt-4'></p>
          <div className='container pt-[60px] px-12 grid md:grid-cols-2 gap-4'>
 
-<div >
-<p className='text-4xl mt-20 font-bold text-start'>
+<div className='' >
+<p className='text-4xl mt-6 font-bold text-start'>
 Our Dream Is  Global Learning Transformation
 </p>
 
-<p className='mt-24 text-start'>Lorem ipsum dolor sit amet consectetur, 
+<p className='mt-14 text-start'>Lorem ipsum dolor sit amet consectetur, 
      adipisicing elit. Minus dolorum animi quo tenetur  
      obcaecati ipsum, suscipit alias, deleniti iusto,  
      cupiditate explicabo. Nihil beatae, inventore consectetur  
@@ -25,23 +25,19 @@ Our Dream Is  Global Learning Transformation
 </div>
 
 
-<div>
-<img className='w-[480px] h-[250px] rounded' src='https://i.ibb.co/vxRK318/about.jpg' alt="about" />
+<div className='container mx-auto'>
+  <img className='w-full h-auto rounded' src='https://i.ibb.co/vxRK318/about.jpg' alt="about" />
 
-<div className='grid md:grid-cols-2 gap-2 mt-6'> 
-   <h1 className='text-3xl font-serif font-semibold text-center'>50+  <small className='text-sm'>Proj</small></h1>
-   <h1 className='text-3xl font-serif font-semibold text-center'>10+  <small className='text-sm'>Clients</small></h1>
-
-
-</div>
-<div className='grid md:grid-cols-2 gap-4 mt-2'> 
-   <h1 className='text-3xl font-serif font-semibold text-center'>70+  <small className='text-sm'>Dev</small></h1>
-   <h1 className='text-3xl font-serif font-semibold text-center'>30+  <small className='text-sm'>Deal</small></h1>
-
-
+ <div className='grid md:grid-cols-2 gap-2 mt-4 py-4'>
+ 
+    <h1 className='text-3xl font-serif font-semibold text-center'>50+ <small className='text-sm'>Proj</small></h1>
+    <h1 className='text-3xl font-serif font-semibold text-center'>70+ <small className='text-sm'>Dev</small></h1> 
+    <h1 className='text-3xl font-serif font-semibold text-center'>30+ <small className='text-sm'>Deal</small></h1>
+    <h1 className='text-3xl font-serif font-semibold text-center'>10+ <small className='text-sm'>Clients</small></h1>
+ 
+ </div>
 </div>
 
-</div>
 
 
         </div>
